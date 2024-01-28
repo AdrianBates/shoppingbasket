f45eddd1af13294a5f1005c9dcc057923c79822f
a_bat <a_bat@DESKTOP-FVKPPEM>
2024-01-28 20:36:32 +0000

    Renamed tests

--- shopppingbasket/src/test/java/com/example/shopppingbasket/discount/ItemDiscountCalculatorTest.java
+++ shopppingbasket/src/test/java/com/example/shopppingbasket/discount/ItemDiscountCalculatorTest.java
@@ -14,28 +14,28 @@ public class ItemDiscountCalculatorTest {
 	private ItemDiscountCalculator discountCalculator = new ItemDiscountCalculator();
 	
 	@Test
-	public void test_calulateDiscountRate_noDiscount() {
+	public void test_calulateDiscount_noDiscount() {
 		OrderLine orderLine = anOrderLine().build();
 		discountCalculator.calculateDiscount(orderLine);
 		assertEquals(0, orderLine.getDiscount());
 	}
 	
 	@Test
-	public void test_calulateDiscountRate_Hat_10PercentDiscount() {
+	public void test_calulateDiscount_Hat_10PercentDiscount() {
 		OrderLine orderLine = anOrderLine().withItem("Hat").build();
 		discountCalculator.calculateDiscount(orderLine);
 		assertEquals(10, orderLine.getDiscount());
 	}
 	
 	@Test
-	public void test_calulateDiscountRate_5Items_20PercentDiscount() {
+	public void test_calulateDiscount_5Items_20PercentDiscount() {
 		OrderLine orderLine = anOrderLine().withQuantity(5).build();
 		discountCalculator.calculateDiscount(orderLine);
 		assertEquals(20, orderLine.getDiscount());
 	}
 	
 	@Test
-	public void test_calulateDiscountRate_5ItemsAndHats_30PercentDiscount() {
+	public void test_calulateDiscount_5ItemsAndHats_30PercentDiscount() {
 		OrderLine orderLine = anOrderLine().withItem("Hat").withQuantity(5).build();
 		discountCalculator.calculateDiscount(orderLine);
 		assertEquals(30, orderLine.getDiscount());
