6bd820ab481fd903c7bc9c5274fe39a1bd069d9c
a_bat <a_bat@DESKTOP-FVKPPEM>
2024-01-28 17:50:38 +0000

    Initial check in

---
+++ shopppingbasket/src/main/java/com/example/shopppingbasket/ShopppingbasketApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopppingbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopppingbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopppingbasketApplication.class, args);
+	}
+
+}
---
+++ shopppingbasket/src/test/java/com/example/shopppingbasket/ShopppingbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopppingbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopppingbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
